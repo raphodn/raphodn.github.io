4e8b8444fea1af27cb846177eacd4e1fd6bd346f
Raphael Odini <raphodn@users.noreply.github.com>
2022-01-11 14:52:53 +0100

    Updates

--- component---src-pages-404-js-03d76632a803423ca8d6.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Bl7J"),u=t("vrFN");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-03d76632a803423ca8d6.js.map
